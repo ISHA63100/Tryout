e72026b25d2c72899a722434c1ec71f9a1885f78
ISHA63100 <74012773+ISHA63100@users.noreply.github.com>
2023-10-17 18:44:01 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,6 +1,36 @@
 import streamlit as st
 import pandas as pd
 import time
+import base64
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded_string});
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def center_buttons():
+    st.markdown(
+        """
+        <style>
+        .stButton {
+            display: flex;
+            justify-content: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 def process_files(uploaded_file):
     if uploaded_file:
@@ -11,7 +41,7 @@ def process_files(uploaded_file):
             st.download_button("Download Duplicated flagged", deduplicated.to_csv(), key='download_duplicates', mime='text/csv')
 
         if st.button("Go To Material Classification"):
-            show_material_classification(df)
+            st.experimental_set_query_params(material_classification=True)  # Switch to Material Classification page
 
 def flag_duplicates(df):
     # Your duplicate flagging logic here
@@ -23,48 +53,35 @@ def process(uploaded_file):
             st.text("Your file is being processed...")
             # Replace this with your actual classification logic
             time.sleep(5)
-            st.text("Processing complete")
+            st.text("Processing complete. Here are the results: ")
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-def show_material_classification(df):
-    if st.button("Back to File Upload"):
-        st.session_state.show_material_classification = False  # Hide Material Classification section
-    else:
-        st.title("Material Classification")
-        uploaded = st.file_uploader("Upload file for material classification", type=["xlsx", "xls", "csv"])
-        if uploaded is not None:
-            file_extension = uploaded.name.split('.')[-1].lower()
-
-            if file_extension in ('xlsx', 'xls'):
-                st.text("Excel file uploaded.")
-                process(uploaded)
-            elif file_extension == 'csv':
-                st.text("CSV file uploaded.")
-                process(uploaded)
-            else:
-                st.text("Unsupported file type. Please upload an Excel file (XLSX or XLS) or a CSV file.")
+def material_classification_page():
+    pass
 
 def main():
+    st.image("eren.png", use_column_width=False, width=100)
     st.title("Microsoft Project")
+    
+    add_bg_from_local("eren.png")  # Replace with the correct file path of your background image
+    
+   
+    
+    
+    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls", "csv"])
 
-    if "show_material_classification" not in st.session_state:
-        st.session_state.show_material_classification = False  # Initialize the flag
+    if uploaded_file is not None:
+        file_extension = uploaded_file.name.split('.')[-1].lower()
 
-    if not st.session_state.show_material_classification:
-        uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls", "csv"])
-        if uploaded_file is not None:
-            file_extension = uploaded_file.name.split('.')[-1].lower()
-
-            if file_extension in ('xlsx', 'xls'):
-                st.text("Excel file uploaded.")
-                process_files(uploaded_file)
-            elif file_extension == 'csv':
-                st.text("CSV file uploaded.")
-                process_files(uploaded_file)
-
-    if st.session_state.show_material_classification:
-        show_material_classification(None)
+        if file_extension in ('xlsx', 'xls'):
+            st.text("Excel file uploaded.")
+            center_buttons()
+            process_files(uploaded_file)
+        elif file_extension == 'csv':
+            st.text("CSV file uploaded.")
+            center_buttons()
+            process_files(uploaded_file)
 
 if __name__ == "__main__":
     main()
