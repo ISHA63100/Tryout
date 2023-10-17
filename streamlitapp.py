db7b858313217355a747fd170041925d03d524f0
ISHA63100 <74012773+ISHA63100@users.noreply.github.com>
2023-10-17 17:09:04 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,45 +1,46 @@
 import streamlit as st
 import pandas as pd
-
-# Define a function to perform keyword matching
-def keyword_matching(data):
-    # Add your keyword matching logic here
-   
-    return matched_data
-
+#with open('styles.css') as f:
+    #st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 def process_files(uploaded_file):
     if uploaded_file:
         df = pd.read_excel(uploaded_file, nrows=30)
-        first_column = df.iloc[:, 0]
-
-        st.dataframe(first_column, height=300)  # Display only the first column with a fixed height
-
-        col1, col2 = st.columns(2)
-
-        if col1.button("Remove Duplicates"):
+        
+        if st.button("Download Duplicate flagged"):
             # Check for duplicates in the first column by default and remove them
-            first_column_no_duplicates = first_column.drop_duplicates()
-            col1.dataframe(first_column_no_duplicates, height=300)  # Display the first column without duplicate values
+            deduplicated = flag_duplicates(df)
+            st.download_button("Download Duplicated flagged", deduplicated.to_csv(), key='download_duplicates', mime='text/csv')
 
-        if col2.button("Submit anyway"):
-            # Call the keyword matching function when "Submit anyway" is clicked
-            matched_data = keyword_matching(first_column)
-            col2.dataframe(matched_data, height=300)  # Display the matched data
+        if st.button("Go To Material Classification"):
+            show_material_classification(df)
+
+def flag_duplicates(df):
+    # Your duplicate flagging logic here
+    pass
+
+def show_material_classification(df):
+    st.title("Material Classification")
+    uploaded_file = st.file_uploader("Upload file for material classification", type=["xlsx", "xls", "csv"])
+    # Your Material Classification page content here
+    # You can create widgets and display relevant information on this page
 
 def main():
-    st.title("First Project")
+    st.title("Microsoft Project")
 
-    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls", "csv"])
 
     if uploaded_file is not None:
         file_extension = uploaded_file.name.split('.')[-1]
 
         if file_extension in ('xlsx', 'xls'):
-            st.text("Excel file uploaded. You can process it here.")
+            st.text("Excel file uploaded.")
             process_files(uploaded_file)
-
+        elif uploaded_file.name.endswith('.csv'):
+            process_files(uploaded_file)
         else:
             st.text("Unsupported file type. Please upload an Excel file (XLSX or XLS).")
+
+
 
 if __name__ == "__main__":
     main()
