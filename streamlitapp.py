27e35832280ceca1219905e5d0fd7d01a0bede4c
ISHA63100 <74012773+ISHA63100@users.noreply.github.com>
2023-10-17 17:53:18 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -11,7 +11,7 @@ def process_files(uploaded_file):
             st.download_button("Download Duplicated flagged", deduplicated.to_csv(), key='download_duplicates', mime='text/csv')
 
         if st.button("Go To Material Classification"):
-            st.experimental_set_query_params(material_classification=True)  # Switch to Material Classification page
+            show_material_classification(df)
 
 def flag_duplicates(df):
     # Your duplicate flagging logic here
@@ -23,43 +23,48 @@ def process(uploaded_file):
             st.text("Your file is being processed...")
             # Replace this with your actual classification logic
             time.sleep(5)
-            st.text("Processing complete. Here are the results: ")
+            st.text("Processing complete")
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-def material_classification_page():
-    st.title("Material Classification")
-    uploaded = st.file_uploader("Upload file for material classification", type=["xlsx", "xls", "csv"])
-    if uploaded is not None:
-        file_extension = uploaded.name.split('.')[-1].lower()
+def show_material_classification(df):
+    if st.button("Back to File Upload"):
+        st.session_state.show_material_classification = False  # Hide Material Classification section
+    else:
+        st.title("Material Classification")
+        uploaded = st.file_uploader("Upload file for material classification", type=["xlsx", "xls", "csv"])
+        if uploaded is not None:
+            file_extension = uploaded.name.split('.')[-1].lower()
 
-        if file_extension in ('xlsx', 'xls'):
-            st.text("Excel file uploaded.")
-            process(uploaded)
-        elif file_extension == 'csv':
-            st.text("CSV file uploaded.")
-            process(uploaded)
-        else:
-            st.text("Unsupported file type. Please upload an Excel file (XLSX or XLS) or a CSV file.")
+            if file_extension in ('xlsx', 'xls'):
+                st.text("Excel file uploaded.")
+                process(uploaded)
+            elif file_extension == 'csv':
+                st.text("CSV file uploaded.")
+                process(uploaded)
+            else:
+                st.text("Unsupported file type. Please upload an Excel file (XLSX or XLS) or a CSV file.")
 
 def main():
     st.title("Microsoft Project")
 
-    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls", "csv"])
+    if "show_material_classification" not in st.session_state:
+        st.session_state.show_material_classification = False  # Initialize the flag
 
-    if uploaded_file is not None:
-        file_extension = uploaded_file.name.split('.')[-1].lower()
+    if not st.session_state.show_material_classification:
+        uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls", "csv"])
+        if uploaded_file is not None:
+            file_extension = uploaded_file.name.split('.')[-1].lower()
 
-        if file_extension in ('xlsx', 'xls'):
-            st.text("Excel file uploaded.")
-            process_files(uploaded_file)
-        elif file_extension == 'csv':
-            st.text("CSV file uploaded.")
-            process_files(uploaded_file)
+            if file_extension in ('xlsx', 'xls'):
+                st.text("Excel file uploaded.")
+                process_files(uploaded_file)
+            elif file_extension == 'csv':
+                st.text("CSV file uploaded.")
+                process_files(uploaded_file)
 
-    # Check if the Material Classification page should be shown
-    if st.experimental_get_query_params().get("material_classification"):
-        material_classification_page()
+    if st.session_state.show_material_classification:
+        show_material_classification(None)
 
 if __name__ == "__main__":
     main()
